00bae1310b7d4d7441683096df8c3ec13f78ada5
guoyongcs <guoyongcs@gmail.com>
2023-06-20 00:28:28 +0800

    train on ciffar

---
+++ EXP_CIFAR/DeepAugment/make_deepaugment.sh
@@ -0,0 +1,4 @@
+python CAE_distort_cifar10.py --total-workers 1 --worker-number 0
+python CAE_distort_cifar100.py --total-workers 1 --worker-number 0
+python EDSR_distort_cifar10.py --total-workers 1 --worker-number 0
+python EDSR_distort_cifar100.py --total-workers 1 --worker-number 0
