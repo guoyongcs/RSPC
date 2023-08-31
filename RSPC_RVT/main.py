fac330904f0b5b2a5c997c5662e0f3656bf9188b
guoyongcs <guoyongcs@gmail.com>
2023-08-31 09:26:12 +0800

    update main.py

--- RSPC_RVT/main.py
+++ RSPC_RVT/main.py
@@ -146,8 +146,7 @@ def get_args_parser():
     parser.add_argument('--pretrained', action='store_true', help='load pretrained model')
 
     # Dataset parameters
-    parser.add_argument('--data-path', default='/datasets01/imagenet_full_size/061417/', type=str,
-                        help='dataset path')
+    parser.add_argument('--data-path', default='', type=str, help='dataset path')
     parser.add_argument('--data-set', default='IMNET', choices=['CIFAR10', 'CIFAR100', 'IMNET', 'INAT', 'INAT19'],
                         type=str, help='Image Net dataset path')
     parser.add_argument('--inat-category', default='name',
